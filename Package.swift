61e71231324f178c7bc0c9ff81ea04a2f8c52b52
Razee <razee.hj@gmail.com>
2023-04-14 11:16:57 +0200

    Update Package

--- Package.swift
+++ Package.swift
@@ -21,14 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DesignSystem",
-            dependencies: [],
             path: "Sources"
-        ),
-        .target(
-            name: "Tokens",
-            resources: [
-                .process("DS.swift")
-            ]
         ),
         .testTarget(
             name: "DesignSystemTests",
