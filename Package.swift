f4767f203ab04bdce5fab1329dbe8c839e986891
Razee <razee.hj@gmail.com>
2023-04-14 11:57:04 +0200

    Update Package

--- Package.swift
+++ Package.swift
@@ -21,7 +21,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DesignSystem",
-            path: "Sources"
+            path: "Sources",
+            resources: [
+                .process("Colors.xcassets")
+            ]
         ),
         .testTarget(
             name: "DesignSystemTests",
--- Sources/DesignSystem/DesignSystem.swift
+++ Sources/DesignSystem/DesignSystem.swift
@@ -4,20 +4,3 @@ public struct DesignSystem {
     public init() {
     }
 }
-
-public class TEST {
-    public private(set) var text = "TEST, World!"
-
-    public init() {
-    }
-}
-
-extension DS {
-    public struct SetText {
-        let text: String
-    }
-}
-
-extension DS.SetText {
-    static let ttttt: Self = .init(text: "TESTTHHSSSS")
-}
