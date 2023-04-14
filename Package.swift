3ed3ca8f503f27ce6f4e307ebbbeff89361828d1
Razee <razee.hj@gmail.com>
2023-04-14 11:54:35 +0200

    Update Package

--- Package.swift
+++ Package.swift
@@ -21,10 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "DesignSystem",
-            path: "Sources",
-            resources: [
-                .process("Colors.xcassets")
-            ]
+            path: "Sources"
         ),
         .testTarget(
             name: "DesignSystemTests",
